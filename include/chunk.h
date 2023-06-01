730e1b1e2005b6e30de29369f99ce8f27224986e
Gustavo Dias De Aguiar <1281523@sga.pucminas.br>
2023-06-01 18:08:31 -0300

    feat: inicial basic structures

---
+++ include/chunk.h
@@ -0,0 +1,26 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+/*
+ * Each instruction has a one-byte operational code
+ * (opcode) that defines its application.
+ */
+typedef enum {
+    OP_RETURN, // Return from the current function.
+} OpCode;
+
+/*
+ * Represents a dynamic array of instructions. Some of
+ * the benefits provided include dense storage, which
+ * is cache-friendly, constant-time indexed element
+ * lookup and appending to the end of the array.
+ */
+typedef struct {
+    int count; // Allocated entries in use.
+    int capacity; // Number of elements allocated.
+    uint8_t *code;
+} Chunk;
+
+#endif
