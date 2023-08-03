c020da8055b6db583561cc55addb607e76cc45a7
gustavodiasag <gustavodias.aguiar1@gmail.com>
2023-08-02 22:26:13 -0300

    feat(Chapter 23): control flow statements

--- src/front-end/compiler.c
+++ src/front-end/compiler.c
@@ -512,6 +512,57 @@ static void expr_stmt()
     emit_byte(OP_POP);
 }
 
+static void for_stmt()
+{
+    begin_scope();
+
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+
+    if (match(TOKEN_SEMICOLON)) {
+        // No initializer.
+    } else if (match(TOKEN_VAR)) {
+        var_declaration();
+    } else {
+        expr_stmt();
+    }
+
+    int loop_start = current_chunk()->count;
+    
+    int exit_jump = -1;
+
+    if (!match(TOKEN_SEMICOLON)) {
+        expression();
+        consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+        // Jump out of the loop if the condition is false.
+        exit_jump = emit_jump(OP_JUMP_FALSE);
+        emit_byte(OP_POP);
+    }
+
+    if (!match(TOKEN_RIGHT_PAREN)) {
+        int body_jump = emit_jump(OP_JUMP);
+        int inc_start = current_chunk()->count;
+
+        expression();
+        emit_byte(OP_POP);
+        consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        emit_loop(loop_start);
+        loop_start = inc_start;
+        patch_jump(body_jump);
+    }
+
+    statement();
+    emit_loop(loop_start);
+
+    // Done only if there is a condition clause.
+    if (exit_jump != -1) {
+        patch_jump(exit_jump);
+        emit_byte(OP_POP);
+    }
+
+    end_scope();
+}
+
 /// @brief Parses a conditional statement.
 static void if_stmt() {
     consume(TOKEN_LEFT_PAREN, "Expect '(' after statement.");
@@ -620,6 +671,8 @@ static void statement()
 {
     if (match(TOKEN_PRINT)) {
         print_stmt();
+    } else if (match(TOKEN_FOR)) {
+        for_stmt();
     } else if (match(TOKEN_IF)) {
         if_stmt();
     } else if (match(TOKEN_WHILE)) {
