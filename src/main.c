869b34d8d357f11456154cf8a07ff0e7258abb6b
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-01-16 14:39:05 -0300

    style: formatting

--- src/main.c
+++ src/main.c
@@ -13,7 +13,6 @@ static void repl()
     
     while (true) {
         printf("> ");
-
         if (!fgets(line, sizeof(line), stdin)) {
             printf("\n");
             break;
@@ -30,7 +29,6 @@ static char* read_file(const char* path)
         fprintf(stderr, "Could not open file \"%s\".\n", path);
         exit(74);
     }
-
     fseek(file, 0L, SEEK_END);
     size_t file_size = ftell(file);
     rewind(file);
@@ -41,14 +39,12 @@ static char* read_file(const char* path)
         fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
         exit(74);
     }
-
     size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
 
     if (bytes_read < file_size) {
         fprintf(stderr, "Could not read file \"%s\".\n", path);
         exit(74);
     }
-
     buffer[bytes_read] = '\0';
     fclose(file);
 
