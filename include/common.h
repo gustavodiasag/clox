cfa865a6eb2bc6993090de8eeb68912c0fefc83a
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-01-18 15:37:14 -0300

    build: define preprocessor flags through build options

--- include/common.h
+++ include/common.h
@@ -7,12 +7,4 @@
 
 #define UINT8_COUNT (UINT8_MAX + 1)
 
-#define NAN_BOXING
-
-// #define DEBUG_PRINT_CODE
-// #define DEBUG_TRACE_EXECUTION
-
-// #define DEBUG_STRESS_GC
-// #define DEBUG_LOG_GC
-
 #endif
